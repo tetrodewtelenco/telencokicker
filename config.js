c56c1c4598f251c7dc7f70468d975ee9f67d1e3a
tetrodewtelenco <tetrodew@gmail.com>
2026-08-23 12:21:48 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TELCO_CONFIG = {
-  SUPABASE_URL: "https://gbsvkzkzvenmrdmrkpii.supabase.co/rest/v1/",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_rhuu_EzkIHWe1JO69gqLDg_NUlS2bqk"
+  SUPABASE_URL: "https://gbsvkzkzvenmrdmrkpii.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "UW_HUIDIGE_PUBLISHABLE_KEY"
 };
